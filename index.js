c1f83299a4097631219825009c121918f214d7e5
JosefKaplan <mail@josefkaplan.com>
2021-04-21 14:37:44 +0200

    update

--- index.js
+++ index.js
@@ -11,30 +11,51 @@ anime.timeline({loop: false})
 let secondText = document.getElementById("secondText");
 anime.timeline({loop: false})
   .add({
-  targets: secondText,
-  opacity: [0,1],
-  easing: "easeInOutQuad",
-  duration: 1500,
-  delay: 1000
-});
+    targets: secondText,
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: 2000
+  });
 
 let thirdText = document.getElementById("thirdText");
 anime.timeline({loop: false})
   .add({
-  targets: thirdText,
-  opacity: [0,1],
-  easing: "easeInOutQuad",
-  duration: 2000,
-  delay: 2300
-});
+    targets: thirdText,
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: 3300
+  });
 
 let fourthText = document.getElementById("fourthText");
 anime.timeline({loop: false})
   .add({
-  targets: fourthText,
-  opacity: [0,1],
-  easing: "easeInOutQuad",
-  duration: 2500,
-  delay: 5000
-});
+    targets: fourthText,
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: 5300
+  });
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  });
 
