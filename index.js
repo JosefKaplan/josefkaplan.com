129c7a7612373a9dcb0a23ca9657217086c786f4
JosefKaplan <mail@josefkaplan.com>
2021-06-22 10:45:50 +0200

    new demo app project added

--- index.js
+++ index.js
@@ -1,53 +1,7 @@
-var t = 600; //Delay time 
-let firstText = document.getElementById("firstText");
-anime.timeline({loop: false})
-  .add({
-  targets: firstText,
-  opacity: [0,1],
-  easing: "easeInOutQuad",
-  duration: 1000,
-  delay: t
-  });
+var t = 750; //time duration
 
-let secondText = document.getElementById("secondText");
-anime.timeline({loop: false})
-  .add({
-    targets: secondTet,
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: t+(t/8)
-  });
-
-let thirdText = document.getElementById("thirdText");
-anime.timeline({loop: false})
-  .add({
-    targets: thirdText,
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: t+(t/2)
-  });
-
-let fourthText = document.getElementById("fourthText");
-anime.timeline({loop: false})
-  .add({
-    targets: fourthText,
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 1000,
-    delay: t+t
-  });
-
-// Wrap every letter in a span
 var textWrapper = document.querySelector('.ml12');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");// Wraps every letter in a span
 
 anime.timeline({loop: false})
   .add({
@@ -56,8 +10,36 @@ anime.timeline({loop: false})
     translateZ: 0,
     opacity: [0,1],
     easing: "easeOutExpo",
-    duration: 500,
-    delay: (el, i) => 500 + 30 * i
+    duration: t,
+    delay: (el, i) => t + 30 * i
   });
 
-  
+  var textWrapper = document.querySelector('.ml13');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");// Wraps every letter in a span
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml13 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: t,
+    delay: (el, i) => t + 30 * i
+  });
+
+
+  var textWrapper = document.querySelector('.ml14');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");// Wraps every letter in a span
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml14 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: t,
+    delay: (el, i) => t + 15 * i
+  });
+
